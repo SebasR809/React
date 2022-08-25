a0b214019f07975727ed8a4383e6ea0de08ef3ac
Kevin Nicolas Ceron Catumba <knceron4@misena.edu.co>
2022-08-25 08:55:48 -0500

    Primer Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import index from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<index />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
